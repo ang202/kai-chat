8ed1808362cacef503f3c5c994f31da8a9d1d86e
Kai <angkaithong2018@gmail.com>
2024-03-12 15:22:58 +0800

    Implement talsec free rasp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:freerasp/freerasp.dart';
 import 'package:kai_chat/config/flavor_config.dart';
 import 'package:kai_chat/core/base/base_binding.dart';
 import 'package:kai_chat/core/routes/app_pages.dart';
@@ -7,9 +9,74 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void mainGlobal() {
+void mainGlobal() async {
   dotenv.load(fileName: FlavorConfig.fileName);
+  await triggerRasp();
   runApp(const MyApp());
+}
+
+void triggerToast({String? error}) {
+  Fluttertoast.showToast(
+      msg: error ?? "",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 16.0);
+}
+
+Future<void> triggerRasp() async {
+  final config = TalsecConfig(
+    /// For Android
+    androidConfig: AndroidConfig(
+      packageName: 'com.kai.chat',
+      signingCertHashes: ['7BHB18WtEfIQa7ZdYPoUjpDOub6HE23TM87nsQ2JMfI='],
+      supportedStores: ['com.sec.android.app.samsungapps'],
+    ),
+
+    /// For iOS
+    iosConfig: IOSConfig(
+      bundleIds: ['com.kai.chat.ios'],
+      teamId: 'asd',
+    ),
+    watcherMail: 'kt.ang@neurogine.com',
+    isProd: true,
+  );
+  await Talsec.instance.start(config);
+  final callback = ThreatCallback(
+      onAppIntegrity: () => {
+            triggerToast(error: "App Integrity"),
+            debugPrint("App integrity"),
+          },
+      onObfuscationIssues: () => {
+            triggerToast(error: "Obfuscation issues"),
+            debugPrint("Obfuscation issues")
+          },
+      onDebug: () => {
+            triggerToast(error: "Debugging"),
+            debugPrint("Debugging"),
+          },
+      onDeviceBinding: () => debugPrint("Device binding"),
+      onDeviceID: () => debugPrint("Device ID"),
+      onHooks: () => debugPrint("Hooks"),
+      onPasscode: () => debugPrint("Passcode not set"),
+      onPrivilegedAccess: () => debugPrint("Privileged access"),
+      onSecureHardwareNotAvailable: () => {
+            triggerToast(error: "Secure hardware not available"),
+            debugPrint("Secure hardware not available")
+          },
+      onSimulator: () => {
+            triggerToast(error: "Simulator"),
+            debugPrint("Simulator"),
+          },
+      onUnofficialStore: () => {
+            triggerToast(error: "Unofficial store"),
+            debugPrint("Unofficial store"),
+          });
+
+  // Attaching listener
+  Talsec.instance.attachListener(callback);
 }
 
 class MyApp extends StatelessWidget {
