48471ede7ffe3c5a4c8ad1c1d92e0a23882ae910
Kai <angkaithong2018@gmail.com>
2025-06-18 17:04:34 +0800

    Include loading view extension

--- lib/core/extensions/view_extensions.dart
+++ lib/core/extensions/view_extensions.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:kai_chat/core/enum/view_state.dart';
 import 'package:kai_chat/core/values/app_colors.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 extension ExpandedWidget on Widget {
   Widget padding(EdgeInsetsGeometry edge) => Padding(
@@ -91,4 +95,27 @@ extension ExpandedWidget on Widget {
 
   Widget ignorePointer([bool ignore = false]) =>
       IgnorePointer(ignoring: ignore, child: this);
+
+  Widget loading(Rx<ViewState> isLoading) => Stack(
+        children: [
+          this,
+          Obx(() => isLoading.value == ViewState.loading
+              ? Container(
+                  color: AppColors.gray100.withValues(alpha: 0.6),
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Center(
+                      child: LoadingAnimationWidget.hexagonDots(
+                    color: AppColors.accent700,
+                    size: 40,
+                  ).repaintBoundary()),
+                )
+              : Container())
+        ],
+      );
+
+  Widget expanded([int flex = 1]) => Expanded(
+        flex: flex,
+        child: this,
+      );
 }
